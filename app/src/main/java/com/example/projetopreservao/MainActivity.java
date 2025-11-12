a4c95000be2f609dde6c573fc688c69889b22ef6
Leonardo <134066125+LeonardoGStrey@users.noreply.github.com>
2025-11-12 08:37:34 -0300

    d

--- app/src/main/java/com/example/projetopreservao/MainActivity.java
+++ app/src/main/java/com/example/projetopreservao/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.projetopreservao;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnMapa, btnInform, btnContribuir;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnMapa = findViewById(R.id.btnMapa);
+        btnMapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mapa = new Intent(getApplicationContext(), mapa.class);
+            }
+        });
+
+        btnInform = findViewById(R.id.btnInform);
+        btnInform.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Informacoes = new Intent(getApplicationContext(), Informacoes.class); 
+            }
+        });
+
+        btnContribuir = findViewById(R.id.btnContribuir);
+        btnContribuir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent contribuir = new Intent(getApplicationContext(), contribuir.class);                                          
+            }
+        });
     }
 }
