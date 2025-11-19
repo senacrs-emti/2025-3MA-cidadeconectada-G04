30b78ab294f242b46ca3f6249adb74d79d471146
Leonardo <134066125+LeonardoGStrey@users.noreply.github.com>
2025-11-19 09:22:28 -0300

    win

--- app/src/main/java/com/example/projetopreservao/Informacoes.java
+++ app/src/main/java/com/example/projetopreservao/Informacoes.java
@@ -2,13 +2,27 @@ package com.example.projetopreservao;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Informacoes extends AppCompatActivity {
+
+    Button btnVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informacoes);
+
+        btnVoltar = findViewById(R.id.btnVoltar);
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent MainActivity = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(MainActivity);
+            }
+        });
     }
 }
